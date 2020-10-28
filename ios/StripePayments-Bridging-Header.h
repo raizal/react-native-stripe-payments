cfeb87837f30224e0530087acaaecd34daf3fae5
ptdede <putu.dede@gmail.com>
2020-10-28 11:07:14 +0800

    Add new iOS stripe bridging

---
+++ ios/StripePayments-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <React/RCTViewManager.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTView.h>
+#import <React/RCTEventEmitter.h>
